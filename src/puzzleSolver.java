4b0bc08bf1d0a29efc4451a6c431767d90c658fc
Unknown <hyma.tiyyagura@gmail.com>
2019-03-15 18:57:34 +0800

    Adding variables to encode Einstein's Puzzle

---
+++ src/puzzleSolver.java
@@ -0,0 +1,45 @@
+/**
+ * Created by hyma on 15/3/19.
+ */
+public class puzzleSolver {
+
+    // converting einstein puzzle to cnf
+
+    public void main (String args[]) {
+        // nationalities
+        int brit = 0;
+        int swede = 1;
+        int dane = 2;
+        int norwegian = 3;
+        int german = 4;
+
+        // house colours
+        int red = 5;
+        int green = 6;
+        int white = 7;
+        int yellow = 8;
+        int blue = 9;
+
+        // pets
+        int dog = 10;
+        int bird = 11;
+        int cat = 12;
+        int horse = 13;
+
+        // beverages
+        int tea = 15;
+        int coffee = 16;
+        int milk = 17;
+        int beer = 18;
+        int water = 19;
+
+        // cigar
+        int pallMall = 20;
+        int dunhill = 21;
+        int blends = 22;
+        int bluemasters = 23;
+        int prince = 24;
+
+    }
+
+}

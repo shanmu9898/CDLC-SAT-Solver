234ca7a54e889ff0ddb3b6dab0a82b59c7895ef6
Unknown <hyma.tiyyagura@gmail.com>
2019-04-21 22:19:25 +0800

    Auto stash before merge of "branchingVariable" and "origin/debug"

--- src/CDCLSolverUpdated.java
+++ src/CDCLSolverUpdated.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.Map;
 
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
